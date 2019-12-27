1b279203e5afc396ced870b340c2125b78a25670
Alberqing <zqysly@qq.com>
2019-12-27 17:07:37 +0800

    redux-saga配置

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,3 +1,10 @@
-function* mySaga () {}
+import { takeEvery } from 'redux-saga/effects';
 
+function* mySaga () {
+    yield takeEvery();  // 两个参数、拼接
+}
+
+function* getList () {
+    // 写逻辑  put方法
+}
 export default mySaga;
