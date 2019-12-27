30f4d8736294a6856e36c9bf5ee1e84ec6826d68
Alberqing <zqysly@qq.com>
2019-12-27 16:49:39 +0800

    redux-saga中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 // import { combineReducers } from 'redux'
 import reducer from './reducer';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import mySaga from './sagas';
 
 // 声明增强函数
 const composeHancer = window.__Redux_DEVTOOLS_EXTENSION_COMPOSE__ ? 
                             window.__Redux_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const hancer = composeHancer(applyMiddleware(thunk));
+const sagaMiddleware = createSagaMiddleware();
+
+const hancer = composeHancer(applyMiddleware(sagaMiddleware));
 
 const store = createStore(reducer, hancer);
+sagaMiddleware.run(mySaga);
 
 export default store;
